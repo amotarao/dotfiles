c9a1a7e8a7fa9b136e91a75a8214d37a7e21cf92
Amon Sawamura <amon.sawamura@gmail.com>
2020-04-04 18:51:14 +0900

    gitignore とか

--- commands/set-symbolic.sh
+++ commands/set-symbolic.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 ln -sf $PWD/symbolic/.gitconfig ~/.gitconfig
+
 mkdir -p ~/.config
 ln -sf $PWD/symbolic/fish ~/.config
+
 mkdir -p ~/Library/Application\ Support/Code/User
 ln -sf $PWD/symbolic/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
--- installs/fish.fish
+++ installs/fish.fish
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/usr/local/bin/fish
 
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 
+fisher add oh-my-fish/theme-bobthefish
 fisher add edc/bass
