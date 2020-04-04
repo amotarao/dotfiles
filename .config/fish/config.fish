9cd4bbd8cff7f2c99cbb0f6ea6307beea44234cb
Amon Sawamura <amon.sawamura@gmail.com>
2020-04-04 18:21:02 +0900

    PATH 整理

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,15 +1,7 @@
 . ~/.config/fish/alias.fish
 
-set PATH $PATH /usr/local/bin
-set PATH $PATH /usr/bin
 set PATH $PATH $HOME/bin
-set PATH $PATH $HOME/work/bin
-set PATH $PATH /bin
-set PATH $PATH /usr/sbin
-set PATH $PATH /sbin
 set PATH $PATH $HOME/.nodebrew/current/bin
-set PATH $PATH $HOME/google-cloud-sdk/bin
-
 set PKG_CONFIG_PATH /usr/X11/lib/pkgconfig/
 
 bass source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc
