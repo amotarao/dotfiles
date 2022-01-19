cda802679d95fbd93446618258c60e392b7b4167
Amon Sawamura <amotarao@gmail.com>
2022-01-19 17:04:53 +0900

    oh-my-fish

--- installs/fisher.fish
+++
@@ -1,6 +0,0 @@
-#!/usr/local/bin/fish
-
-curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-
-fisher add oh-my-fish/theme-bobthefish
-fisher add edc/bass
---
+++ installs/oh-my-fish.fish
@@ -0,0 +1,6 @@
+#!/usr/local/bin/fish
+
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+
+omf install bobthefish
+omf install bass
