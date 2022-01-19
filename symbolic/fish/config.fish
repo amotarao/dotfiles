4c02ccaf10ae81c6785e53dc75103da961bcf217
Amon Sawamura <amotarao@gmail.com>
2022-01-19 18:10:15 +0900

    remove path

--- symbolic/fish/config.fish
+++ symbolic/fish/config.fish
@@ -5,9 +5,6 @@ end
 
 . ~/.config/fish/alias.fish
 
-set PATH $PATH $HOME/bin
-set PATH $PATH $HOME/.nodebrew/current/bin
-set PKG_CONFIG_PATH /usr/X11/lib/pkgconfig/
 
-bass source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc
-bass source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc
+# bass source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc
+# bass source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc
