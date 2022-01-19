140d6a8122c0001e5aebc213faa87a9d69689b17
Amon Sawamura <amotarao@gmail.com>
2022-01-19 17:47:29 +0900

    fish

--- commands/set-symbolic.sh
+++ commands/set-symbolic.sh
@@ -4,8 +4,9 @@ ln -sf $PWD/symbolic/.gitconfig ~/.gitconfig
 ln -sf $PWD/symbolic/.bash_profile ~/.bash_profile
 ln -sf $PWD/symbolic/.bashrc ~/.bashrc
 
-# mkdir -p ~/.config
-# ln -sf $PWD/symbolic/fish ~/.config
+mkdir -p ~/.config
+ln -sf $PWD/symbolic/fish ~/.config
+ln -sf $PWD/symbolic/omf ~/.config
 
 mkdir -p ~/Library/Application\ Support/Code/User
 ln -sf $PWD/symbolic/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
--- symbolic/fish/config.fish
+++ symbolic/fish/config.fish
@@ -1,3 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    nodenv init -|psub
+end
+
 . ~/.config/fish/alias.fish
 
 set PATH $PATH $HOME/bin
