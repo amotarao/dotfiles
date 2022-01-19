f7d44d4252440cd95112a72cbd2877a338c3c885
Amon Sawamura <amotarao@gmail.com>
2022-01-19 17:23:00 +0900

    load .bash_profile, .bashrc

--- commands/set-symbolic.sh
+++ commands/set-symbolic.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 ln -sf $PWD/symbolic/.gitconfig ~/.gitconfig
+ln -sf $PWD/symbolic/.bash_profile ~/.bash_profile
+ln -sf $PWD/symbolic/.bashrc ~/.bashrc
 
-mkdir -p ~/.config
-ln -sf $PWD/symbolic/fish ~/.config
+# mkdir -p ~/.config
+# ln -sf $PWD/symbolic/fish ~/.config
 
 mkdir -p ~/Library/Application\ Support/Code/User
 ln -sf $PWD/symbolic/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
