7a15e63aa8cadc3fa5d76d890c35eacca46c7cd3
Amon Sawamura <amotarao@gmail.com>
2025-12-02 21:00:12 +0900

    indent 揃える

--- installs/vscode.sh
+++ installs/vscode.sh
@@ -2,7 +2,7 @@
 
 cat $PWD/configs/vscode-extensions.txt | while read line
 do
- code --install-extension $line
+  code --install-extension $line
 done
 
 code --list-extensions > $PWD/configs/vscode-extensions.txt
